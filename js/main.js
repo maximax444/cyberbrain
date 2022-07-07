90dd6b454dada38179bd6219900ef31b3a312a92
Max <max.kuzn.msx@gmail.com>
2022-07-07 17:48:54 +0300

    desktop version of the homepage without modal

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(".home-portfolio__slider").slick({
+    slide: ".home-portfolio__wrap",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: ".home-portfolio__prev",
+    nextArrow: ".home-portfolio__next"
+});
