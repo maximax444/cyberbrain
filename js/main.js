4bf40991312e3f1b24e62640571521498338cb32
Max <max.kuzn.msx@gmail.com>
2022-07-11 00:49:35 +0300

    add mobile version of the homepage with a few bugs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.preloader').hide();
+        new WOW({
+            mobile: false,
+        }).init();
+        printTitle();
+    }, 1000);
+
+
+});
+function printTitle() {
+    let title = $('.main__title');
+    let titletext = title.text().trim();
+    title.html('');
+    let i = 0;
+    let int = setInterval(function () {
+
+        title.html(title.html() + titletext[i]);
+        i++;
+        if (i >= titletext.length) {
+            clearInterval(int);
+        }
+    }, 50);
+}
+if ($(window).width() <= 767) {
+    $('.after').on('click', function () {
+        $('.header').toggleClass('active');
+    });
+    $('.header__logo').html('<img src="' + $('.header__logo').attr('data-mob') + '">');
+}
 $(".home-portfolio__slider").slick({
     slide: ".home-portfolio__wrap",
     slidesToShow: 1,
@@ -6,3 +37,21 @@ $(".home-portfolio__slider").slick({
     prevArrow: ".home-portfolio__prev",
     nextArrow: ".home-portfolio__next"
 });
+
+if (screen.width >= 1200) {
+    let bg = $('.home-about .after');
+    let bg2 = $('.home-brands .after');
+    let bg3 = $('.home-modules .after');
+    let bg4 = $('.home-portfolio .after');
+    let bg5 = $('.main .before');
+    window.addEventListener('mousemove', function (e) {
+        let x = e.clientX / window.innerWidth;
+        let y = e.clientY / window.innerHeight;
+        bg.css('transform', 'translate(-' + x * 100 + 'px, -' + y * 100 + 'px)');
+        bg2.css('transform', 'translate(-' + x * 100 + 'px, -' + y * 100 + 'px)');
+        bg3.css('transform', 'translate(-' + x * 100 + 'px, -' + y * 100 + 'px)');
+        bg4.css('transform', 'translate(-' + x * 100 + 'px, -' + y * 100 + 'px)');
+        bg5.css('transform', 'translate(-' + x * 30 + 'px, -' + y * 30 + 'px)');
+
+    });
+}
